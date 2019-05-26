ef00b89c548c2022d03cdc5727f380d03cd4e820
martinRenou <martin.renou@gmail.com>
2019-05-26 13:55:03 +0200

    Remove ReplaceAlphaNode call

--- js/lib/src/BlockUtils/PlugIns/Threshold.js
+++ js/lib/src/BlockUtils/PlugIns/Threshold.js
@@ -73,8 +73,44 @@ class Threshold extends PlugInBlock {
   }
 
   _process () {
-    // Set the threshold node that will hide part of the skin
-    this._setThresholdNode();
+    // Min/max values of the input
+    this.inputDataMin = this.getComponentMin(this._inputDataName,
+      this._inputComponentNames);
+    this.inputDataMax = this.getComponentMax(this._inputDataName,
+      this._inputComponentNames);
+
+    if (this._lowerBound === undefined) {
+      this._lowerBound = this.inputDataMin;
+    }
+    if (this._upperBound === undefined) {
+      this._upperBound = this.inputDataMax;
+    }
+
+    // Create lowerbound and upperbound float nodes
+    this._lowerBoundNode = new THREE.FloatNode(this._lowerBound);
+    this._upperBoundNode = new THREE.FloatNode(this._upperBound);
+
+    // GLSL's STEP function is more optimized than an if statement
+    // It will returns 0 if inputData > upperBound, 1 otherwise
+    this.isUnderUpperThreshold = new THREE.Math2Node(
+      this._inputDataNode,
+      this._upperBoundNode,
+      THREE.Math2Node.STEP
+    );
+
+    // It will returns 0 if inputData < lowerBound, 1 otherwise
+    this.isOverLowerThreshold = new THREE.Math2Node(
+      this._lowerBoundNode,
+      this._inputDataNode,
+      THREE.Math2Node.STEP
+    );
+
+    // Create alpha node
+    this.thresholdAlpha = new THREE.OperatorNode(
+      this.isUnderUpperThreshold,
+      this.isOverLowerThreshold,
+      THREE.OperatorNode.MUL
+    );
 
     this._CPUCompute = this.parentBlock.tetraArray !== undefined;
 
@@ -114,50 +150,6 @@ class Threshold extends PlugInBlock {
   }
 
   /**
-   * Function that set Threshold node
-   * **/
-  _setThresholdNode(){
-    // Min/max values of the input
-    this.inputDataMin = this.getComponentMin(this._inputDataName,
-      this._inputComponentNames);
-    this.inputDataMax = this.getComponentMax(this._inputDataName,
-      this._inputComponentNames);
-
-    if (this._lowerBound === undefined) {
-      this._lowerBound = this.inputDataMin;
-    }
-    if (this._upperBound === undefined) {
-      this._upperBound = this.inputDataMax;
-    }
-
-    // Create lowerbound and upperbound float nodes
-    this._lowerBoundNode = new THREE.FloatNode(this._lowerBound);
-    this._upperBoundNode = new THREE.FloatNode(this._upperBound);
-
-    // GLSL's STEP function is more optimized than an if statement
-    // It will returns 0 if inputData > upperBound, 1 otherwise
-    let isUnderUpperThreshold = new THREE.Math2Node(
-      this._inputDataNode,
-      this._upperBoundNode,
-      THREE.Math2Node.STEP
-    );
-
-    // It will returns 0 if inputData < lowerBound, 1 otherwise
-    let isOverLowerThreshold = new THREE.Math2Node(
-      this._lowerBoundNode,
-      this._inputDataNode,
-      THREE.Math2Node.STEP
-    );
-
-    // Create alpha node
-    this.thresholdAlpha = new THREE.OperatorNode(
-      isUnderUpperThreshold,
-      isOverLowerThreshold,
-      THREE.OperatorNode.MUL
-    );
-  }
-
-  /**
    * Initialize input for threshold effect
    * @param {string} dataName - Name of the input data of which you want
    * to set input components for this plug-in
@@ -169,13 +161,10 @@ class Threshold extends PlugInBlock {
 
     if (this._processed && this.thresholdAlpha) {
       // Update material
-      let oldNode = this.thresholdAlpha;
+      this.isUnderUpperThreshold.a = this._inputDataNode;
+      this.isOverLowerThreshold.b = this._inputDataNode;
 
-      // Update the Warp Node
-      this._setThresholdNode();
-
-      // Replace the old node with the new Warp Node in materials
-      this.replaceAlphaNode(oldNode, this.thresholdAlpha);
+      this.updateMaterial();
 
       // Update geometry
       if (this._CPUCompute) {
